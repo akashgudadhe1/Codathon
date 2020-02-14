ccd63ac5edca6fea27bdc3d92758dfd797ac621d
Akash Gudadhe <akashgudadhe112@gmail.com>
2020-02-14 09:25:45 +0530

    Aggregation queries added

--- hrank/sql/select.sql
+++ hrank/sql/select.sql
@@ -1,3 +1,4 @@
+#1. BASIC SELECT QUERIES
 # https://www.hackerrank.com/challenges/revising-the-select-query/problem
 SELECT * FROM CITY 
  WHERE COUNTRYCODE='USA' AND
@@ -29,3 +30,26 @@ SELECT CITY, STATE FROM STATION;
 # https://www.hackerrank.com/challenges/weather-observation-station-3/problem
 SELECT DISTINCT CITY FROM STATION
   WHERE (ID%2=0);
+
+#2. AGGREGATION QUERIES
+# https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+SELECT COUNT(NAME) FROM CITY 
+  WHERE POPULATION > 100000;
+
+# https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+SELECT SUM(POPULATION) FROM CITY
+  WHERE DISTRICT='California';
+
+# https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+SELECT AVG(POPULATION) FROM CITY
+  WHERE DISTRICT='California';
+
+# https://www.hackerrank.com/challenges/average-population/problem
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
+
+# https://www.hackerrank.com/challenges/japan-population/problem
+SELECT SUM(POPULATION) FROM CITY
+ WHERE COUNTRYCODE='JPN';
+
+# https://www.hackerrank.com/challenges/population-density-difference/problem
+SELECT MAX(POPULATION)-MIN(POPULATION) FROM CITY;
