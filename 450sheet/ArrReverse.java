ca393d4280e141b01676fb5e27ec171c750a222d
Akash Gudadhe <akashgudadhe112@gmail.com>
2021-04-03 12:25:48 +0530

    array reverse

---
+++ 450sheet/ArrReverse.java
@@ -0,0 +1,22 @@
+public class ArrReverse{
+    static void reverseArray(int arr[], int start, int end){
+        int temp;
+        while(start<end){
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[]={1, 2, 3, 4, 5, 6, 7, 10};
+        reverseArray(arr, 0, arr.length-1);
+    }
+}
