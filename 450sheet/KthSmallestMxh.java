4085bdc508685421abeea614b82dd1cff46aa9fa
Akash Gudadhe <akashgudadhe112@gmail.com>
2021-04-04 08:05:39 +0530

    -1

--- 450sheet/KthSmallestMxh.java
+++ 450sheet/KthSmallestMxh.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class KthSmallestMxh{
     public static void main(String args[]){
-        int arr[]={7, 10, 4, 3, 20, 15};
-        int k=3; 
+        int arr[]={7, 10, 4, 3, 20, 15}; //3, 4, 7, 10, 15, 20
+        int k=4; 
         PriorityQueue<Integer> maxh = new PriorityQueue<Integer>();
-        for(int i=0; i<arr.length-1; i++){
+        for(int i=0; i<arr.length; i++){
             maxh.add(arr[i]);
 
             if(maxh.size() > k){
